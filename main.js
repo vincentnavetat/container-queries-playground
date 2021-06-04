fa3bb8635781d3455e44813b86c47d3f9f1bb205
Vincent Navetat <vincent.navetat@carwow.co.uk>
2021-06-05 00:25:25 +0100

    Add draggable and sorta make it werk

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 // import a CSS module
 import classes from './main.css';
 
+import { Swappable, Plugins } from '@shopify/draggable';
+
 export default () => {
-  console.log(classes.main);
+  const containers = document.querySelectorAll('.wrapper');
+
+  const swappable = new Swappable(containers, {
+    draggable: '.kirby',
+    mirror: {
+      constrainDimensions: true,
+    },
+    plugins: [Plugins.ResizeMirror],
+  });
+
+  console.log('kewl!');
+  console.log(swappable);
 };
