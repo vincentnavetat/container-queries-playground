b13aab0c1cd3f5f3dd4fb2eb3ad0ebcb27a58f0c
Vincent Navetat <vincent.navetat@carwow.co.uk>
2021-06-05 14:05:51 +0100

    Droppable weeeeeeeeerks

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 // import a CSS module
 import classes from './main.css';
 
-import { Swappable, Plugins } from '@shopify/draggable';
+import { Droppable } from '@shopify/draggable';
 
 export default () => {
-  const containers = document.querySelectorAll('.wrapper');
+  const containers = document.querySelectorAll('.container');
 
-  const swappable = new Swappable(containers, {
+  new Droppable(containers, {
     draggable: '.kirby',
+    dropzone: '.wrapper',
     mirror: {
       constrainDimensions: true,
     },
-    plugins: [Plugins.ResizeMirror],
   });
-
-  console.log('kewl!');
-  console.log(swappable);
 };
