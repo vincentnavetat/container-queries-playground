c69f7a487af96f791de7838116be22c1a24f4fd4
Vincent Navetat <vincent.navetat@carwow.co.uk>
2021-06-10 10:56:24 +0100

    Add transitions

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 // import a CSS module
 import classes from './main.scss';
 
-import { Droppable } from '@shopify/draggable';
+import { Droppable, Plugins } from '@shopify/draggable';
 
 export default () => {
   const containers = document.querySelectorAll('.container');
 
-  new Droppable(containers, {
+  const droppable = new Droppable(containers, {
     draggable: '.kirby',
     dropzone: '.wrapper',
     mirror: {
       constrainDimensions: true,
     },
+    plugins: [Plugins.ResizeMirror],
   });
 };
